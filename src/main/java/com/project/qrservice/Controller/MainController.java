13390728a18d75a7818c35913b924d1c902001d3
Ariel <vilcheariel2@gmail.com>
2024-12-09 21:12:58 -0300

    commit

--- src/main/java/com/project/qrservice/Controller/MainController.java
+++ src/main/java/com/project/qrservice/Controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
@@ -20,11 +21,16 @@ public class MainController {
     }
 
     @GetMapping("/qrcode")
-    public ResponseEntity<BufferedImage> qrCode(){
+    public ResponseEntity<BufferedImage> qrCode(@RequestParam int size, String type){
         BufferedImage image = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = image.createGraphics();
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 250, 250);
+        if (size < 350 && size > 150) {
+            g.fillRect(0, 0, size, size);
+        } else if(type.equals("png")){
+
+        }
+
         return ResponseEntity.ok()
                 .contentType(MediaType.IMAGE_PNG)
                 .body(image);
